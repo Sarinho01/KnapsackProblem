987b8e09b275336afd3674c9a0c85314d1d75741
Sarinho01 <alanvictor.sarinho@gmail.com>
2023-11-20 16:51:50 -0300

    implemented knapsack problem

---
+++ src/main/java/org/saros/KnapsackProblem.java
@@ -0,0 +1,52 @@
+package org.saros;
+
+public class KnapsackProblem {
+
+    private int backPackSize;
+    private int[] itemsWeight;
+    private int[] itemsProfit;
+    
+    public KnapsackProblem(int backpackSize, int[] itemsWeight, int[] itemsProfit) {
+        this.backPackSize = backpackSize;
+        this.itemsProfit = itemsProfit;
+        this.itemsWeight = itemsWeight;
+    }
+
+    public int solve() {
+        //  Quantidade de itens
+        int n = itemsWeight.length;
+
+        //  Matriz quantidade de itens x peso da mochila
+        int[][] answer = new int[n + 1][backPackSize + 1];
+        int currentItemWeight;
+
+        for (int i = 1; i < answer.length; i++) {
+
+            //  J é o peso atual
+            for (int j = 1; j < backPackSize; j++) {
+                //  Peso do item atual
+                currentItemWeight = itemsWeight[i - 1];
+
+                //  Caso o peso do item seja maior do que o peso atual (não cabe na mochila)
+                //  Então é colocado o valor da linha (item) anterior com o mesmo peso J (mesma coluna)
+                if (currentItemWeight > j) {
+                    answer[i][j] = answer[i - 1][j];
+                }
+                //  Caso o item caiba na mochila
+                //  É colocado como valor atual nessa posição da matriz o maior entre
+                //  O valor do item + o valor na linha anterior do peso restante
+                //  Ou o valor da linha anterior nesse mesmo peso
+                else {
+                    answer[i][j] = Math.max(
+                            answer[i - 1][j],
+                            itemsProfit[i - 1] + answer[i - 1][j - currentItemWeight]
+                    );
+
+                }
+            }
+        }
+
+        //  A resposta é a última linha na última coluna
+        return answer[n][backPackSize - 1];
+    }
+}
---
+++ src/test/java/KnapsackProblemTest.java
@@ -0,0 +1,59 @@
+import org.saros.KnapsackProblem;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class KnapsackProblemTest {
+
+    @Test
+    void test01NormalCase() {
+        int[] itemsProfit = {1, 2, 3};
+        int[] itemsWeight = {4, 5, 1};
+        int backpackSize = 3;
+        KnapsackProblem knapsackProblem = new KnapsackProblem(backpackSize, itemsWeight, itemsProfit);
+
+        int actual = knapsackProblem.solve();
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void test02DoesNotFitAnyItem() {
+        int[] itemsProfit = {1, 2, 3};
+        int[] itemsWeight = {4, 5, 6};
+        int backpackSize = 3;
+        KnapsackProblem knapsackProblem = new KnapsackProblem(backpackSize, itemsWeight, itemsProfit);
+
+        int actual = knapsackProblem.solve();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void test03NormalCase() {
+        int[] itemsProfit = {60, 100, 120};
+        int[] itemsWeight = {10, 20, 30};
+        int backpackSize = 3;
+        KnapsackProblem knapsackProblem = new KnapsackProblem(backpackSize, itemsWeight, itemsProfit);
+
+        int actual = knapsackProblem.solve();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void test04NormalCase() {
+        int[] itemsProfit = {6, 10, 12, 8, 5};
+        int[] itemsWeight = {1, 2, 3, 5, 4};
+        int backpackSize = 9;
+        KnapsackProblem knapsackProblem = new KnapsackProblem(backpackSize, itemsWeight, itemsProfit);
+
+        int actual = knapsackProblem.solve();
+        int expected = 28;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
